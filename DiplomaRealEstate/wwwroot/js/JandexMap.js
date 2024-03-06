6485702a79b389a0e48f6320ce01dbfe524a4e35
deffil5453 <nozdrin_vitya@mail.ru>
2024-03-06 17:16:17 +0300

    234

--- DiplomaRealEstate/wwwroot/js/JandexMap.js
+++ DiplomaRealEstate/wwwroot/js/JandexMap.js
@@ -1,9 +1,10 @@
 ﻿function initMap() {
     ymaps.ready(function () {
-        var myMap = new ymaps.Map("map", {
-            center: [47.516905, 42.209749],
-            zoom: 5
-        });
+        var myMap = new ymaps.Map("map",
+            {
+                center: [47.516905, 42.209749],
+                zoom: 5
+            });
 
         myMap.events.add('click', function (e) {
             var coords = e.get('coords');
@@ -14,14 +15,14 @@
 
             window.myPlacemark = new ymaps.Placemark(coords);
             myMap.geoObjects.add(window.myPlacemark);
-            
+
             ymaps.geocode(coords).then(function (res) {
                 var firstGeoObject = res.geoObjects.get(0);
-                
+
                 var addressComponents = firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData.Address.Components');
                 var region = '';
                 var city = '';
-                
+
                 addressComponents.forEach(function (component) {
                     if (component.kind === 'province') {
                         region = component.name;
@@ -31,10 +32,9 @@
                     }
                 });
 
-                var address = firstGeoObject.getAddressLine();
-
-                // Передача данных в Blazor
-                DotNet.invokeMethodAsync('DiplomaRealEstate', 'ReceiveMapData', coords[0].toString(), coords[1].toString(), region, city);
+                DotNet.invokeMethodAsync('DiplomaRealEstate', 'ReceiveMapData',
+                    coords[0].toString(), coords[1].toString(), region, city);
+                    
             });
         });
     });
